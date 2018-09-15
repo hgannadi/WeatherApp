b4ba478340f4a21615623a5e20212e529f15a4dd
hgannadi <hgannadi@gmail.com>
2018-09-15 14:14:25 +0200

    Initial Commit

---
+++ WeatherApp/WeatherApp/WeatherPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using Xamarin.Forms;
+
+namespace WeatherApp
+{
+    public partial class WeatherPage : ContentPage
+    {
+        public WeatherPage()
+        {
+            InitializeComponent();
+
+            //Set the default binding to a default object for now
+            BindingContext = new Weather();
+        }
+
+        //private async void GetWeatherBtn_Clicked(object sender, EventArgs e)
+        //{
+        //    Weather weather = await Core.GetWeather("60601");
+        //    getWeatherBtn.Text = weather.Title;
+        //}
+
+        private async void GetWeatherBtn_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(zipCodeEntry.Text))
+            {
+                Weather weather = await Core.GetWeather(zipCodeEntry.Text);
+                BindingContext = weather;
+                getWeatherBtn.Text = "Search Again";
+            }
+        }
+    }
+}
